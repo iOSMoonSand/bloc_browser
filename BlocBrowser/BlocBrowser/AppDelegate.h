ad941534e3230983d5223308a67641d797418004
iOSMoonSand <alexisschreier@gmail.com>
2016-05-17 15:02:50 -0700

    Checkpoint 23 completed

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Alexis Schreier on 05/17/16.
+//  Copyright © 2016 Alexis Schreier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
