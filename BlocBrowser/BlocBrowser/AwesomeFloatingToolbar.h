e4e364a4e38b9afa30300f48e5042452b35a4c1c
iOSMoonSand <alexisschreier@gmail.com>
2016-05-18 11:44:25 -0700

    changes to master that were meant to be on checkpoint 25

---
+++ BlocBrowser/BlocBrowser/AwesomeFloatingToolbar.h
@@ -0,0 +1,29 @@
+//
+//  AwesomeFloatingToolbar.h
+//  BlocBrowser
+//
+//  Created by Alexis Schreier on 05/18/16.
+//  Copyright © 2016 Alexis Schreier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AwesomeFloatingToolbar;
+
+@protocol AwesomeFloatingToolbarDelegate <NSObject>
+
+@optional
+
+- (void) floatingToolbar:(AwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle:(NSString *)title;
+
+@end
+
+@interface AwesomeFloatingToolbar : UIView
+
+- (instancetype) initWithFourTitles:(NSArray *)titles;
+
+- (void) setEnabled:(BOOL)enabled forButtonWithTitle:(NSString *)title;
+
+@property (nonatomic, weak) id <AwesomeFloatingToolbarDelegate> delegate;
+
+@end
